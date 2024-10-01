ff9ec000bbd1c18bbc0748f49d3aa540eeffe641
Menescal <daniel.menescal@snowmanlabs.com>
2024-10-01 11:16:32 -0300

    chore: change folder customer data

--- app.module.ts
+++ app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { CustomerModule } from 'src/customer.module'
+import { CustomerModule } from 'src/customer/customer.module'
 
 @Module({
     imports: [
